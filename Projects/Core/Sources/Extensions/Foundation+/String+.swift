f3f34fabac032fd4eb4e17fe08d72a6a4f4cb145
ChoiAnYong <dksdyd78@naver.com>
2026-02-10 18:27:33 +0900

    feat: #18 - 국가 코드 변환 로직 구현

---
+++ Projects/Core/Sources/Extensions/Foundation+/String+.swift
@@ -0,0 +1,34 @@
+//
+//  String+.swift
+//  Core
+//
+//  Created by 최안용 on 1/31/26.
+//  Copyright © 2026 NDGL-iOS. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+    func toFlag() -> String {
+        guard self.count == 2 else { return "🏳️" }
+        
+        let base: UInt32 = 127397
+        var flagString = ""
+        
+        for uni in self.uppercased().unicodeScalars {
+            if let scalar = UnicodeScalar(base + uni.value) {
+                flagString.append(String(scalar))
+            } else {
+                return "🏳️"
+            }
+        }
+        return flagString
+    }
+    
+    func toKoreanCountryName() -> String {
+        guard self.count == 2 else { return "알 수 없음"}
+        
+        let locale = Locale(identifier: "ko_KR")
+        return locale.localizedString(forRegionCode: self) ?? "알 수 없음"
+    }
+}
