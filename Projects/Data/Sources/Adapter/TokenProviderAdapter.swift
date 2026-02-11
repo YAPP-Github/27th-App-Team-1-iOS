ccc40494169133f027d2e5a7eeb74464730b2b45
ChoiAnYong <dksdyd78@naver.com>
2026-02-12 03:31:54 +0900

    chore: #18 - 임시 accessToken

--- Projects/Data/Sources/Adapter/TokenProviderAdapter.swift
+++ Projects/Data/Sources/Adapter/TokenProviderAdapter.swift
@@ -18,6 +18,7 @@ public final class TokenProviderAdapter: TokenProviding, @unchecked Sendable {
     }
 
     public func accessToken() -> String? {
-        tokenRepository.get(.accessToken)
+//        tokenRepository.get(.accessToken)
+        "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYmE3ODIwYS0wMDUzLTQxZDctODdhYi00Zjk2ZWM3ZDI1MTMiLCJpYXQiOjE3NzA3OTUwMjYsImV4cCI6MTc3MDg4MTQyNn0.oNCkotV0uA-3kCtTwGhTUwA9fUUhuO85p1k3952oTfRaULOw2Ix3RpXq_ta82ynmUK7F3i8F0Jb1d4_Rl-zcgA"
     }
 }
