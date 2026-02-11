d73fd164ce6ab1eaee88b404a4363310db653622
ChoiAnYong <dksdyd78@naver.com>
2026-02-12 03:26:08 +0900

    feat: #18 - Search RIBS 구현

---
+++ Projects/Features/SearchFeature/Sources/SearchInteractor.swift
@@ -0,0 +1,50 @@
+//
+//  SearchInteractor.swift
+//  SearchFeature
+//
+//  Created by 최안용 on 2/7/26.
+//  Copyright © 2026 NDGL-iOS. All rights reserved.
+//
+
+import RIBs
+import RxSwift
+
+public protocol SearchRouting: ViewableRouting {
+    
+}
+
+protocol SearchPresentable: Presentable {
+    var listener: SearchPresentableListener? { get set }
+    // TODO: Declare methods the interactor can invoke the presenter to present data.
+}
+
+public protocol SearchListener: AnyObject {
+    func detachSearch()
+}
+
+final class SearchInteractor: PresentableInteractor<SearchPresentable>, SearchInteractable, SearchPresentableListener {
+
+    weak var router: SearchRouting?
+    weak var listener: SearchListener?
+
+    // TODO: Add additional dependencies to constructor. Do not perform any logic
+    // in constructor.
+    override init(presenter: SearchPresentable) {
+        super.init(presenter: presenter)
+        presenter.listener = self
+    }
+
+    override func didBecomeActive() {
+        super.didBecomeActive()
+        // TODO: Implement business logic here.
+    }
+
+    override func willResignActive() {
+        super.willResignActive()
+        // TODO: Pause any business logic.
+    }
+    
+    func detachSearch() {
+        listener?.detachSearch()
+    }
+}
