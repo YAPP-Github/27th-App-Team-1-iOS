bb7bf31697b06669f1fca58263af2098b8bc9796
ChoiAnYong <dksdyd78@naver.com>
2026-02-14 18:14:22 +0900

    refactor: #28 - api 그룹에 따른 RepositoryProtocol 구현

---
+++ Projects/Domain/Sources/Interface/TravelTemplate/TravelTemplateRepositoryProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  TravelTemplateRepositoryProtocol.swift
+//  Domain
+//
+//  Created by 최안용 on 2/14/26.
+//  Copyright © 2026 NDGL-iOS. All rights reserved.
+//
+
+import Foundation
+
+public protocol TravelTemplateRepositoryProtocol {
+    func fetchPlaces(travelId: Int, day: Int) async throws -> [TravelPlace]
+    func fetchTravelDetail(id: Int) async throws -> TravelDetail
+    func searchTemplate() async throws -> Int
+    func fetchPopularTripList(id: Int?, page: Int?, size: Int?) async throws -> [TripInfo]
+    func fetchRecommendTripList(page: Int?, size: Int?) async throws -> [TripInfo]
+}
