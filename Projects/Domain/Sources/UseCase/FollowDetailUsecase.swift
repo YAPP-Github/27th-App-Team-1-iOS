5711c760a97362ea762cfdd5bd91ede6c8f76b7f
ChoiAnYong <dksdyd78@naver.com>
2026-02-16 19:16:02 +0900

    feat: #28 - FollowDetailUsecase 구현

---
+++ Projects/Domain/Sources/UseCase/FollowDetailUsecase.swift
@@ -0,0 +1,55 @@
+//
+//  FollowDetailUsecase.swift
+//  Domain
+//
+//  Created by 최안용 on 2/16/26.
+//  Copyright © 2026 NDGL-iOS. All rights reserved.
+//
+
+import Foundation
+
+public protocol FollowDetailUsecaseProtocol {
+    func fetchTravelDetail(id: Int) async throws -> TravelDetail
+    func fetchPlaces(travelId: Int, day: Int) async throws -> [TravelPlace]
+    func createUserTravel(request: CreateTravelRequest) async throws -> CreateTravelResponse
+    func fetchPlaceDetail(googlePlaceId: String) async throws -> PlaceDetail
+    func fetchPlacePhotos(googlePlaceId: String) async throws -> [PlacePhoto]
+}
+
+public final class FollowDetailUsecase {
+    private let travelTemplateRepository: TravelTemplateRepositoryInterface
+    private let userTravelRepository: UserTravelRepositoryInterface
+    private let placeRepository: PlaceRepositoryInterface
+    
+    public init(
+        travelTemplateRepository: TravelTemplateRepositoryInterface,
+        userTravelRepository: UserTravelRepositoryInterface,
+        placeRepository: PlaceRepositoryInterface
+    ) {
+        self.travelTemplateRepository = travelTemplateRepository
+        self.userTravelRepository = userTravelRepository
+        self.placeRepository = placeRepository
+    }
+}
+
+extension FollowDetailUsecase: FollowDetailUsecaseProtocol {
+    public func fetchTravelDetail(id: Int) async throws -> TravelDetail {
+        try await travelTemplateRepository.fetchTravelDetail(id: id)
+    }
+    
+    public func fetchPlaces(travelId: Int, day: Int) async throws -> [TravelPlace] {
+        try await travelTemplateRepository.fetchPlaces(travelId: travelId, day: day)
+    }
+    
+    public func createUserTravel(request: CreateTravelRequest) async throws -> CreateTravelResponse {
+        try await userTravelRepository.createUserTravel(request: request)
+    }
+    
+    public func fetchPlaceDetail(googlePlaceId: String) async throws -> PlaceDetail {
+        try await placeRepository.fetchPlaceDetail(googlePlaceId: googlePlaceId)
+    }
+    
+    public func fetchPlacePhotos(googlePlaceId: String) async throws -> [PlacePhoto] {
+        try await placeRepository.fetchPlacePhotos(googlePlaceId: googlePlaceId)
+    }
+}
