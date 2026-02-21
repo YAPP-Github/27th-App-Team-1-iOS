5cc6aaea37170112517e720be15d7ead443a12e5
kimnahun <rlaskgns0212@naver.com>
2026-02-22 05:28:00 +0900

    fix: nullable 추가 대응

--- Projects/Features/HomeFeature/Sources/Models/HomePresentationModel.swift
+++ Projects/Features/HomeFeature/Sources/Models/HomePresentationModel.swift
@@ -90,14 +90,14 @@ extension HomePresentationModel.Schedule {
 extension MyTripSummary {
     func toPresention() -> HomePresentationModel.Banner {
         let schedule: HomePresentationModel.Schedule
-        if let tripSchedule = self.tripSchedule {
+        if let s = self.tripSchedule {
             schedule = .init(
-                id: tripSchedule.id,
-                day: tripSchedule.day,
-                placeName: tripSchedule.placeName,
-                thumbnailUrl: tripSchedule.thumbnailUrl,
-                transport: tripSchedule.transport,
-                estimatedDuration: tripSchedule.estimatedDuration
+                id: s.id,
+                day: s.day,
+                placeName: s.placeName,
+                thumbnailUrl: s.thumbnailUrl,
+                transport: s.transport,
+                estimatedDuration: s.estimatedDuration
             )
         } else {
             schedule = .empty
